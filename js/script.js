e1b1e99cfab5c1833b902f3508d8c26a919e7ae0
59013-21-03 <59013-21-03@exchange.ad.afpanet>
2024-01-08 10:31:40 +0100

    testsend form

---
+++ js/script.js
@@ -0,0 +1,8 @@
+//animation texte home
+const typed = new Typed('.multiple-text',{
+    strings: ['Fullstack Developer', 'Chef'],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+  });
